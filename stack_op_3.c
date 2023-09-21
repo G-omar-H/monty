44d9e31afd9650b295118698470974713b452909
G-omar-H <omarghazi12@gmail.com>
2023-09-21 23:03:13 +0100

    you\'re comment is you\'re right

--- stack_op_3.c
+++ stack_op_3.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include <stdio.h>
 /**
  * nop - continue.
  * @stack: top stack tracker
@@ -10,4 +11,26 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 	return;
+}
+/**
+ * pchar - prints the character value of the the top of a stack.
+ * @stack: top stack tracker
+ * @line_number: line number track
+ * Return: void.
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+    if (!arg->head)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_all_args();
+		exit(EXIT_FAILURE);
+	}
+    else if (!isascii(arg->head->n))
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_all_args();
+		exit(EXIT_FAILURE);
+	}
+    printf("%c\n", arg->head->n);
 }
